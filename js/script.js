437e991ffc60b706f7eed39a3ce186ddf6339c0d
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2024-11-23 23:22:56 +0100

    JS TERMINADO

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@ const boton = document.getElementById("fetchJoke");
 const uList = document.getElementById("jokeList");
 const url = "https://api.chucknorris.io/jokes/random"; 
 
-//Traer Chiste
+// Cargar lo creado en DOM y LocalStorage
+document.addEventListener("DOMContentLoaded", () => {
+    const arrayChistes = JSON.parse(localStorage.getItem("chisteAle")) || [];
+    arrayChistes.forEach((chiste) =>{
+        uList.innerHTML += `
+        <div class="chiste">
+            <li>${chiste}</li>
+            <button class="borrar">Borrar Chiste</button>
+        </div>
+        `;
+    })
+})
+
+// Traer Chiste
 boton.addEventListener("click", () => {
     fetch(url)
         .then((response) => {
@@ -12,13 +25,15 @@ boton.addEventListener("click", () => {
             return response.json()
         })
         .then((data) => {
-            console.log(data);
-            localStorage.setItem("chisteAle", `${data.value}`)
+            
+            // Mi local Storage con chistes 
+            let arrayChistes = JSON.parse(localStorage.getItem("chisteAle")) || [];// Con Parse Recupero de mi Array
+            arrayChistes.push(data.value); // Los Agrego a mi Array 
+            localStorage.setItem("chisteAle", JSON.stringify(arrayChistes)); // Los Grabo en el Local
+
             uList.innerHTML += `
             <div class="chiste">
-            <li>
-            ${localStorage.getItem("chisteAle")}
-            </li>
+            <li>${data.value}</li>
             <button id="borrar">Borrar Chiste</button>
             </div>
             `
@@ -29,3 +44,18 @@ boton.addEventListener("click", () => {
             console.error("Error Inesperado", Error)
         })
 })
+
+console.log(localStorage);
+
+// Cargar lo creado en DOM y LocalStorage
+uList.addEventListener("click", (event) => { // le doy parametro event para abajo capturarlo con el target
+    if (event.target.classList.contains("borrar")) {// target especifica que afectara unicamente al elemento que yo seleccione con clase "borrar" no a todos
+        const divPadre = event.target.parentElement // Obtengo el contenedor
+        const borrarChiste = divPadre.querySelector("li").textContent; // Obtengo el li del contenedor 
+        
+        let arrayChistes = JSON.parse(localStorage.getItem("chisteAle")) || [];
+        arrayChistes = arrayChistes.filter((chiste) => chiste !== borrarChiste);
+        localStorage.setItem("chisteAle", JSON.stringify(arrayChistes));
+    divPadre.remove();
+    }
+    })
